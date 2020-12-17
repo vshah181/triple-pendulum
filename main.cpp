2be74538203c3d81a1b812df3d2090ee87510a43
vshah181 <63469297+vshah181@users.noreply.github.com>
2020-12-17 14:18:47 +0000

    Define convention

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
  * Language standard: C++ 20
  * Authors: Jiten Dhandha and Varun Shah
 *******************/
+
+// PascalCase for classes and structs
+// snake_case for variable and objects
+// camelCase for functions
+// Try and keep namespaces to one word beginning with lowercase letter
 #include <iostream>
 
 int main() {
