56fd046c3ddf9ca7a305238663595dc5b7688933
vshah181 <63469297+vshah181@users.noreply.github.com>
2020-12-17 14:21:24 +0000

    Add style for filename

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 // snake_case for variable and objects
 // camelCase for functions
 // Try and keep namespaces to one word beginning with lowercase letter
+// Single word filenames starting in lowecase
 #include <iostream>
 
 int main() {
